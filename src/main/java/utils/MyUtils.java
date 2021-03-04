94964640e49dd60f292ca9fb3b8669ef9b337472
Ravindra Narava <nravindra2k@gmail.com>
2021-03-03 23:14:49 -0600

    Initial commit

---
+++ src/main/java/utils/MyUtils.java
@@ -0,0 +1,39 @@
+//class to convert string to camelcase
+package utils;
+
+public class MyUtils 
+{ 
+ 
+	public static String convert(String s) 
+	{ 
+		
+ 
+		
+		int cnt= 0; 
+		int n = s.length(); 
+		char ch[] = s.toLowerCase().toCharArray(); 
+		int res_ind = 0; 
+
+		for (int i = 0; i < n; i++) 
+		{  
+			if (ch[i] == ' ') 
+			{ 
+				cnt++;  
+				ch[i + 1] = Character.toUpperCase(ch[i + 1]); 
+				continue; 
+			} 
+ 
+			else
+				ch[res_ind++] = ch[i]; 
+		} 
+
+		return String.valueOf(ch, 0, n - cnt); 
+	} 
+ 
+	public static void main(String args[]) 
+	{ 
+		String str = "I get intern at geeksforgeeks"; 
+		System.out.println(convert(str)); 
+	} 
+} 
+ 
